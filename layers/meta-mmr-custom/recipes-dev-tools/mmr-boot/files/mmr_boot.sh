b5ac982fff0f29a23b5b01ab7cf457efbacb3b88
PigneInTesta <giovannitassotti165@gmail.com>
2026-07-08 17:49:51 +0200

    Update mmr_boot.sh to launch the mmr_kria_logger after received the /orin/on

--- layers/meta-mmr-custom/recipes-dev-tools/mmr-boot/files/mmr_boot.sh
+++ layers/meta-mmr-custom/recipes-dev-tools/mmr-boot/files/mmr_boot.sh
@@ -3,3 +3,4 @@
 source /etc/profile.d/ros/setup.bash
 while [[ $(ros2 topic info /orin/on &> /dev/null; echo $?) -ne 0 ]]; do :; done
 ros2 launch as_manager as_manager_launch.py &
+ros2 launch mmr_kria_logger mmr_kria_logger_launch.py &
